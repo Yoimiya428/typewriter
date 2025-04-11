c4cd0b2f854017ca4a5818d1d62ea437116babe4
BoxinCao <ngxmex@gmail.com>
2025-04-10 22:44:49 -0400

    function fixed

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,8 @@
 const sentence = "hello there from lighthouse labs";
 
-// for (const char of sentence) {
-//   process.stdout.write(`${char}\n`);
-// }
-
-setTimeout(() => {
-  for (const char of sentence) {
+for (const char of sentence) {
+  setTimeout(() => {
     process.stdout.write(`${char}\n`);
-  }
-  
-}, 50) // <= 1s delay to make it noticeable. Can dial it down later.
+  }, 50 ); // <= 1s delay to make it noticeable. Can dial it down later.
+
+} 
